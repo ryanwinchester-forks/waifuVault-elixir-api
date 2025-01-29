47ec90024f6052b4d7058b91b290edb1e73cee27
John Baylor <john.baylor@gmail.com>
2025-01-29 11:55:07 -0800

    Handle slow error cases

--- lib/waifu_vault.ex
+++ lib/waifu_vault.ex
@@ -3,20 +3,68 @@ defmodule WaifuVault do
   Documentation for `WaifuVault`.
   """
 
-  @request_options (Mix.env == :test) && Req.new(
-    base_url: "https://waifuvault.moe/rest",
-    plug: {Req.Test, WaifuVault}
-  ) || Req.new(
-    base_url: "https://waifuvault.moe/rest"
-  )
-
+  @request_options (Mix.env() == :test &&
+                      Req.new(
+                        base_url: "https://waifuvault.moe/rest",
+                        plug: {Req.Test, WaifuVault}
+                      )) || Req.new(base_url: "https://waifuvault.moe/rest")
 
   @doc """
-  Tells the server to create a bucket. duh.
+  Buckets are virtual collections that are linked to your IP and a token. When you create a bucket, you will receive a bucket token that you can use in Get Bucket to get all the files in that bucket
+
+  NOTE: Only one bucket is allowed per client IP address, if you call it more than once, it will return the same bucket token
+
+  To create a bucket, use the create_bucket function. This function does not take any arguments.
+
+  ```
+  require WaifuVault
+  bucket = WaifuVault.create_bucket()
+  print(bucket.token)
+  ```
   """
   def create_bucket() do
-    {:ok, response} = Req.get(@request_options, url: "/bucket/create")
+    case Req.get(@request_options, url: "/bucket/create") do
+      {:ok, %Req.Response{status: 200, body: body}} -> {:ok, bucket_response_from_map(body)}
+      {:ok, response} -> {:error, "Error #{response.status} (#{response.status}): {response.body}"}
+      {:error, error} -> {:error, error.reason}
+    end
+  end
 
-    response.body
+  def bucket_response_from_map(map) do
+    %{
+      token: map["token"],
+      files: Enum.map(map["files"] || [], &file_response_from_map/1),
+      albums: Enum.map(map["albums"] || [], &album_response_from_map/1)
+    }
+  end
+
+  def file_response_from_map(map) do
+    %{
+      token: map["token"],
+      url: map["url"],
+      retentionPeriod: map["retentionPeriod"],
+      bucket: map["bucket"],
+      views: map["views"],
+      album: album_response_from_map(map["album"] || %{}),
+      options: file_options_from_map(map["options"] || %{})
+    }
+  end
+
+  def album_response_from_map(map) do
+    %{
+      files: Enum.map(map["files"] || [], &file_response_from_map/1),
+      token: map["token"],
+      bucketToken: map["bucketToken"],
+      publicToken: map["publicToken"],
+      name: map["name"],
+    }
+  end
+
+  def file_options_from_map(map) do
+    %{
+      hideFilename: map["hideFilename"] || false,
+      oneTimeDownload: map["oneTimeDownload"] || false,
+      protected: map["oneTimeDownload"] || false
+    }
   end
 end
--- test/waifu_vault_test.exs
+++ test/waifu_vault_test.exs
@@ -8,11 +8,35 @@ defmodule WaifuVaultTest do
         Req.Test.json(conn, %{"token" => "server token"})
       end)
 
-      bucket = WaifuVault.create_bucket()
+      {:ok, bucketResponse} = WaifuVault.create_bucket()
 
-      refute is_nil(bucket)
-      refute is_nil(bucket["token"])
-      assert bucket["token"] == "server token"
+      refute is_nil(bucketResponse)
+      assert bucketResponse.token == "server token"
+      assert bucketResponse.files == []
+      assert bucketResponse.albums == []
+    end
+  end
+
+  # Way too slow due to 3 1-second retries
+  describe "slow error cases" do
+    test "timeout" do
+      Req.Test.stub(WaifuVault, fn conn ->
+        Req.Test.transport_error(conn, :timeout)
+      end)
+
+      {:error, error} = WaifuVault.create_bucket()
+
+      assert error == :timeout
+    end
+
+    test "internal server error" do
+      Req.Test.stub(WaifuVault, fn conn ->
+        Plug.Conn.send_resp(conn, 500, "internal server error")
+      end)
+
+      {:error, error} = WaifuVault.create_bucket()
+
+      assert error == "Error 500 (500): {response.body}"
     end
   end
 end
